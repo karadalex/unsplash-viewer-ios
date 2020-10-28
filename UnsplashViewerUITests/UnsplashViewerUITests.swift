4c5649bc7a3545d39eaa43acac05672976004146
Alexios Karadimos <karadalex@gmail.com>
2020-10-28 17:06:02 +0200

    setup fastlane for tests and screenshots

--- UnsplashViewerUITests/UnsplashViewerUITests.swift
+++ UnsplashViewerUITests/UnsplashViewerUITests.swift
@@ -25,10 +25,12 @@ class UnsplashViewerUITests: XCTestCase {
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        snapshot("0Launch")
     }
 
     func testLaunchPerformance() throws {
