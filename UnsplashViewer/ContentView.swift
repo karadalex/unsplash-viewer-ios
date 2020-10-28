61d3e51dd0aa0bf18c8d2280a218bbfd126e30c2
Alexios Karadimos <karadalex@gmail.com>
2020-10-28 16:21:41 +0200

    list images

--- UnsplashViewer/ContentView.swift
+++ UnsplashViewer/ContentView.swift
@@ -5,12 +5,41 @@
 //  Created by Alexios Karadimos on 28/10/20.
 //
 
+import Combine
 import SwiftUI
 
+
+class DataSource: ObservableObject {
+    // Send nothing and never throw errors
+    let didChange = PassthroughSubject<Void, Never>()
+    @Published var pictures = [String]()
+    
+    init() {
+        let fm = FileManager.default
+        
+        if let path = Bundle.main.resourcePath,
+           let items = try? fm.contentsOfDirectory(atPath: path) {
+            for item in items {
+                if item.hasSuffix("-unsplash.jpg") {
+                    pictures.append(item)
+                }
+            }
+        }
+        
+        didChange.send(())
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var dataSource = DataSource()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(dataSource.pictures, id: \.self) { picture in
+                Text(picture)
+                    .padding()
+            }.navigationTitle(Text("Unsplash Viewer"))
+        }
     }
 }
 
